ef4ee43753d7501487d873881f31f7c475715224
zxn <zxn288912@163.com>
2023-08-22 18:41:20 +0800

    feat: add plugin-git

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,12 +1,13 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+import { gitPlugin } from '@vuepress/plugin-git'
 
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '你好，生活！',
     description: '这是我的第一个 VuePress 站点',
-    plugins: [backToTopPlugin()],
+    plugins: [backToTopPlugin(), gitPlugin()],
     // charset: 'utf-8',
     theme: defaultTheme({
         sidebar: [
