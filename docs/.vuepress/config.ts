f047553d74c3c2fd31fd8358995bae60834813bd
zxn2889 <zxn288912@163.com>
2023-10-09 15:36:13 +0800

    ci: reset

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,7 @@ import sidebar from './sidebar'
 export default defineUserConfig({
     base: '/record-moments/',
     lang: 'zh-CN',
-    title: '你好，生活！',
+    title: '你好，生活，撒贝宁！',
     description: 'The more recorded, the better.',
     plugins: [backToTopPlugin(), gitPlugin()],
     // charset: 'utf-8',
