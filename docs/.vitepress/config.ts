d1c98ae89c700d383fad9a93d6bacf5c70efdf61
zxn2889 <zxn288912@163.com>
2024-07-15 17:56:42 +0800

    ci: update pkg

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    base: '/record-moments/',
+    lang: 'zh-CN',
+    title: '你好，生活！',
+    description: 'The more recorded, the better.',
+    markdown: {
+        lineNumbers: true
+    }
+})
