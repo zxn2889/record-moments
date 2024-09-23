f273172c34ad06345adb37b114dd52c7e5b86d7a
zxn2889 <zxn288912@163.com>
2024-09-23 11:06:03 +0800

    ci: update config and del some img

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,14 +6,31 @@ export default defineConfig({
     lang: 'zh-CN',
     title: '火箭吧！',
     description: 'The more recorded, the better.',
-    markdown: {
-        lineNumbers: true
-    },
     cleanUrls: true,
     metaChunk: true,
     lastUpdated: true,
+    markdown: {
+        lineNumbers: true
+    },
+    head: [
+        ['link', { rel: 'icon', type: 'image/svg+xml', href: '/record-moments/img/vitepress-logo-mini.svg' }],
+        ['link', { rel: 'icon', type: 'image/png', href: '/record-moments/img/vitepress-logo-mini.png' }],
+        ['link', { rel: 'mask-icon', color: '#3eaf7c', href: '/record-moments/img/vitepress-logo.svg' }],
+        ['link', { rel: 'apple-touch-icon', href: '/record-moments/img/vitepress-logo.png' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { property: 'og:type', content: 'website' }],
+        ['meta', { property: 'og:locale', content: 'en' }],
+        ['meta', { property: 'og:title', content: '火箭吧 | 一个记录美好生活的网站' }],
+        ['meta', { property: 'og:site_name', content: '火箭吧' }],
+        ['meta', { property: 'og:image', content: 'https://vitepress.dev/vitepress-og.jpg' }],
+        ['meta', { property: 'og:url', content: 'https://vitepress.dev/' }],
+    ],
     themeConfig: {
-        logo: '/hero.png',
+        logo: '/img/vitepress-logo-mini.svg',
+        socialLinks: [
+            { icon: 'github', link: 'https://zxn2889.github.io/record-moments/' },
+        ],
+        carbonAds: { code: 'CEBDT27Y', placement: 'vuejsorg' },
         sidebar,
     },
     rewrites: {
